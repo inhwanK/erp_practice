aa0a8c6435313737e48bb1d7404bc2472570cb5c
inhwanK <inhan1009@naver.com>
2021-03-20 22:25:35 +0900

    TitlePanel Morph 함. TitleTablePanel 만들기 전.

---
+++ src/main/java/erp_practice/ui/table/TitleTablePanel.java
@@ -0,0 +1,14 @@
+package erp_practice.ui.table;
+
+import javax.swing.JPanel;
+
+public class TitleTablePanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public TitleTablePanel() {
+
+	}
+
+}
