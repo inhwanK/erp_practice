e9f4232e7bd62469e83e48bf61cd37d08ee80658
inhwanK <inhan1009@naver.com>
2021-03-25 21:11:13 +0900

    JTable 만들면서 블로그에 정리중.

---
+++ src/main/java/erp_practice/ui/table/AbstractTablePanel.java
@@ -0,0 +1,5 @@
+package erp_practice.ui.table;
+
+public abstract class AbstractTablePanel {
+
+}
--- src/main/java/erp_practice/ui/table/TitleTablePanel.java
+++ src/main/java/erp_practice/ui/table/TitleTablePanel.java
@@ -1,14 +1,31 @@
 package erp_practice.ui.table;
 
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class TitleTablePanel extends JPanel {
-
-	/**
-	 * Create the panel.
-	 */
+	private String[] columnNames = {"직책코드","직책명"};
+	private Object[][] data = {
+			{1,"사장"},
+			{2,"부장"},
+			{3,"과장"},
+			{4,"대장"},
+			{5,"사원"},
+	};
+	private JTable table;
+	
 	public TitleTablePanel() {
 
+		initialize();
+	}
+	private void initialize() {
+		
+		setLayout(new BorderLayout(0, 0));
+		
+		table = new JTable(data, columnNames);
+		add(table, BorderLayout.NORTH);
 	}
 
 }
